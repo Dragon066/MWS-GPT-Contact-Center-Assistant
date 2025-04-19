3cd9f2186050ec312599385aa98aa7f250318561
Dragon066 <NickTitov123@yandex.ru>
2025-04-20 00:06:20 +0300

    Add qdrant logic

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,7 +1,10 @@
+import io
+
 import aiohttp
 from core import push_record
 from db import Database
-from fastapi import FastAPI
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from qdrant import create_collection_from_file, get_collection, is_collection_exists
 
 app = FastAPI()
 
@@ -37,3 +40,29 @@ async def get_chat_info(chat_id: int):
     except Exception as ex:
         data = {"error": str(ex)}
     return data
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        raise HTTPException(status_code=400, detail="Поддерживаются только CSV файлы")
+
+    # try:
+    contents = await file.read()
+
+    await create_collection_from_file("database", io.StringIO(contents.decode("utf-8")))
+
+    return {
+        "status": "success",
+        "message": "File uploaded successfully",
+    }
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/collection_size")
+async def get_collection_size():
+    if is_collection_exists("database"):
+        return {"points_count": get_collection("database").points_count}
+    else:
+        return {"error": "no collection"}
---
+++ app/backend/qdrant.py
@@ -0,0 +1,66 @@
+import os
+
+import pandas as pd
+from openai import OpenAI
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, PointStruct, VectorParams
+
+LLM_API_KEY = os.getenv("LLM_API_KEY")
+LLM_BASE_URL = os.getenv("LLM_BASE_URL")
+LLM_EMBEDDING_MODEL = os.getenv("LLM_EMBEDDING_MODEL")
+LLM_EMBEDDING_VECTOR_SIZE = os.getenv("LLM_EMBEDDING_VECTOR_SIZE")
+
+llm_client = OpenAI(api_key=LLM_API_KEY, base_url=LLM_BASE_URL)
+qdrant_client = QdrantClient(host="qdrant", port=6333)
+
+
+def get_embeds(texts: list[str], batch_size: int = 128):
+    result = []
+    for i in range(0, len(texts), batch_size):
+        emb = llm_client.embeddings.create(
+            model=LLM_EMBEDDING_MODEL,
+            input=texts[i : i + batch_size],
+        )
+        result.extend(emb.data)
+
+    return result
+
+
+async def create_collection_from_file(collection_name: str, file_csv):
+    df = pd.read_csv(file_csv, index_col=0)
+
+    if is_collection_exists(collection_name):
+        qdrant_client.delete_collection(collection_name)
+
+    qdrant_client.create_collection(
+        collection_name=collection_name,
+        vectors_config=VectorParams(
+            size=LLM_EMBEDDING_VECTOR_SIZE, distance=Distance.COSINE
+        ),
+    )
+
+    vectorized = get_embeds(df.iloc[:, 0].to_list())
+
+    points = [
+        PointStruct(
+            id=i, vector=vectorized[i].embedding, payload=df.iloc[i, 1:].to_dict()
+        )
+        for i in range(len(df))
+    ]
+
+    qdrant_client.upload_points(collection_name=collection_name, points=points)
+
+    return True
+
+
+def is_collection_exists(collection_name: str):
+    try:
+        return qdrant_client.get_collection(collection_name) is not None
+    except Exception:
+        return False
+
+
+def get_collection(collection_name: str):
+    if not is_collection_exists(collection_name):
+        raise ValueError(f"Collection {collection_name} not found")
+    return qdrant_client.get_collection(collection_name)
